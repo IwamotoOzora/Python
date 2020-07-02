a4c7150dc891076f36b755418c8d4d48d2f88705
Iwamoto Ozora <o.iwamoto@a-force.co.jp>
2020-07-02 18:39:18 +0900

    refs #8:python1_test4

--- python1_test4.py
+++ python1_test4.py
@@ -1,14 +1,14 @@
 debt = 250000
 repayment = 30000
-rate = 14
+rate = (1 + 14 / 12 / 100)
 month = 0
 
-while debt > repayment:
-    debt = debt * (1 + rate / 12 / 100) - repayment
+while debt > 0:
+    debt = debt * rate - repayment
     month += 1
     print(str(month) + "ヶ月目:返済額=" + str(repayment) +"円,"\
     + "残り" + str(debt) + "円")
-debt = debt * (1 + rate / 12 / 100)
-month += 1
-print(str(month) + "ヶ月目:返済額=" + str(debt) + "円," + "返済完了。")
+    if debt < repayment:
+          break
+print(str(month + 1) + "ヶ月目:返済額=" + str(debt * rate) + "円," + "返済完了。")
 
