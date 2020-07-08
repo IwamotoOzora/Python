d46e0d21ce947b07f131810c8229de7cb34fceff
Iwamoto Ozora <o.iwamoto@a-force.co.jp>
2020-07-09 08:49:08 +0900

    refs #21:selenium課題修正

--- selenium_test.py
+++ selenium_test.py
@@ -1,35 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys as keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_binary
 from time import sleep
 
 driver = webdriver.Chrome()
 driver.get("https://a-force.biz/")
 
+wait = WebDriverWait(driver, 10)
+
 try:
     # チャットボットを出す
-    search_chat_button = driver.find_element_by_id("button-area")
-    search_chat_button.click()
+    open_chat_bot_element = driver.find_element_by_id("button-area")
+    open_chat_bot_element.click()
 
     driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
 
-    sleep(3)
-
     #チャットボットへ挨拶をする
-    search_chat_form = driver.find_element_by_id("tweet")
-    search_chat_form.send_keys("おはよう")
-    search_send_button = driver.find_element_by_id("send_btn")
-    search_send_button.click()
-
-    sleep(3)
+    input_element = driver.find_element_by_id("tweet")
+    input_element.send_keys("おはよう" + keys.ENTER)
 
     # チャットボットへ質問する
-    search_chat_form.send_keys("社風")
-    search_send_button.click()
-
-    sleep(5)
+    element = wait.until(EC.presence_of_element_located((By.XPATH,\
+         "//*[@id='field']/div[3]/div[2]/div[2]/div")))
+    input_element.send_keys("社風" + keys.ENTER)
 
     # 質問の答えを出力する
+    element = wait.until(EC.presence_of_element_located((By.XPATH,\
+         "//*[@id='field']/div[5]/div[2]/div[2]/div")))
     message = driver.find_elements_by_class_name("alert")[4].text 
     print(message)
 
